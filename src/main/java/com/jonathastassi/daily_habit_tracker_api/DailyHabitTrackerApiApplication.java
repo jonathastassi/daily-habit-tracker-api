fb52b2f30fdb5e8f4194ec6becc95f77b264725f
Jonathas Tassi <jonathastassi@hotmail.com>
2024-11-05 21:55:51 -0300

    Created project

---
+++ src/main/java/com/jonathastassi/daily_habit_tracker_api/DailyHabitTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.jonathastassi.daily_habit_tracker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyHabitTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyHabitTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonathastassi/daily_habit_tracker_api/DailyHabitTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathastassi.daily_habit_tracker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyHabitTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
